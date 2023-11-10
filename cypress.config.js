1b796b7e00b532d8b829889554a7ef5f2d29a19d
Arik Jones <github@tnypxl.com>
2023-11-10 02:54:36 -0600

    * Update config to flatten the folder structure. * Add scripts to package.json

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://demo.banked.com/",
+    baseUrl: "https://demo.banked.com",
     specPattern: "tests/**/*.spec.js",
-    fixturesFolder: "fixtures",
-    supportFolder: "support",
-    downloadsFolder: "downloads",
+    fixturesFolder: "./fixtures",
+    supportFolder: "./support",
+    supportFile: "./support/e2e.js",
+    downloadsFolder: "./downloads",
+    screenshotsFolder: "./screenshots",
+    headless: true,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
